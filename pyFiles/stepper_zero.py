21625ec2f9464397cf5dd8852cdfc912caec0362
patrickkok <pkok1996@gmail.com>
2021-07-16 16:08:14 +0800

    dispense func

--- pyFiles/stepper_zero.py
+++ pyFiles/stepper_zero.py
@@ -37,35 +37,46 @@ def turn_stepper(deg, direction):
         sleep(delay)
         STEP.off()
         sleep(delay)
-        print(x)
     SLEEP.off()
-    print('done')
+    print('turnt')
     #add in an update of the current pos in json file
 
 def turn_servo(pos):
     pi = pigpio.pi()
-    pi.set_servo_pulsewidth(24,pos)
+    if pos == 1500:
+        for k in range (500, pos+1, 50):
+            pi.set_servo_pulsewidth(24,k)
+            sleep(0.1)
+    elif pos == 500:
+        for l in range (1500, pos-1, -50):
+            pi.set_servo_pulsewidth(24,l)
+            sleep(0.1)
+    
     sleep(1)
     pi.set_servo_pulsewidth(24,0)
     
-turn_servo(500)
-print('done 500')
-turn_servo(2500)
-print('done 2000')
+# turn_servo(500)
+# sleep(3)
+# print('done 500')
+# for j in range(0,501, 50):
+#     turn_servo(500+j)
+#     sleep(0.05)
+# turn_servo(1000)
 
 def lower_nozzle():
     pi = pigpio.pi()
     pi.set_servo_pulsewidth(17,2000)
+    sleep(1) #delete later
     PUMP.on()
     VALVE.off()
     try:
         for i in range(0, 1001, 10):
-            print(DIST.value)
             pi.set_servo_pulsewidth(17, 2000-i)
             sleep(0.3)
             if DIST.value == 0:
                 pi.set_servo_pulsewidth(17, 2000)
-                #pi.set_servo_pulsewidth(17, 0)
+                sleep(2)
+                pi.set_servo_pulsewidth(17, 0)
                 break
         
     except KeyboardInterrupt:
@@ -76,39 +87,40 @@ def lower_nozzle():
 def calc_turn_angle(current, destination):
     ang = destination - current
     if 0 <= ang <= 6:
-        return ang,CW
+        return (30*ang),CW
     elif 7 <= ang <= 11:
         ang = 12-ang
-        return ang,CCW
+        return (30*ang),CCW
     elif -11 <= ang <= -7:
         ang = 12 + ang
-        return ang,CW
-    elif -6 <= ang <-1:
+        return (30*ang),CW
+    elif -6 <= ang <=-1:
         ang = (-1)*ang
-        return ang,CCW
+        return (30*ang),CCW
 
-def dispense(med_id):
+def dispense(med_id, qty):
     default = 500
-    dispense = 2500
-    with open('/home/pi/Documents/Medbox_GUI/container.json', 'r') as f:
+    dispense = 1500
+    with open('/home/pi/Documents/MedBox/pyFiles/container.json', 'r') as f:
         container = json.load(f)
     current = int(container['current_pos'])
     print(current)
     for i in range(1,13):
-        container_med_id = container['container_'+str(i)]['med_id']
+        container_med_id = container['container_'+str(i)]['medicine']['id']
         if container_med_id == med_id:
             destination = i
             break
     print(destination)
-    turn_angle = calc_turn_angle(current, destination)
-    print(turn_angle)
-    turn_stepper(turn_angle[0], turn_angle[1])
+    ang, dire = calc_turn_angle(current, destination)
+    print(ang, dire)
+    turn_stepper(ang, dire)
     lower_nozzle() #turns on pump and lowers vacuum nozzle, the nozzle will rise after getting clsoe to a pill
     turn_servo(dispense)#moves nozzle over the dispensing area
     VALVE.on()
     sleep(1)
-    VALVE.off()
+    VALVE.on()
     PUMP.off()
+    VALVE.off()
     turn_servo(default)
     #update pill amount in json
     return None
@@ -116,6 +128,7 @@ def dispense(med_id):
 import os
 # os.system('sudo pigpiod')
 
+dispense(51,1)
 
 
 
@@ -124,3 +137,4 @@ import os
 
 
 
+ 
